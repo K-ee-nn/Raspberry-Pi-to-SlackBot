03003894800b5c36ec496ca315cedcb523dad4b1
K-ee-nn <kenf.connect@gmail.com>
2023-08-01 10:44:35 +0800

    Updated with no raspberry pi code

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import slack
 import os
 from dotenv import load_dotenv
 import time
-import serial
-
-# initialize serial port
-serial1 = serial.Serial('/dev/ttyUSB0', 9600)
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # load your API token
 env_path = '.env'
@@ -15,11 +13,13 @@ client = slack.WebClient(os.environ['SLACK_BOT_TOKEN'])
 # send message to channel for 1000 times before breaking
 count = 0
 while count <= 1e3:
-    datastring = serial1.readline()
+    datastring = "Hi"
     try:
+        print(f"Try Writing {datastring} to channel...")
         client.chat_postMessage(channel="#test-channel", text=datastring)
     except Exception as e:
         print(e)
     count += 1
     time.sleep(2)
-serial.close()
+
+print("Program exited...")
